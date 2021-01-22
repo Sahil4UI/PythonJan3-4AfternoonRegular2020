b3545448cbf17afe20836069558e52d66288c375
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-01-22 15:59:34 +0530

    Update imdb crawler.py

--- imdb crawler.py
+++ imdb crawler.py
@@ -1,3 +1,4 @@
+'''
 #Web CrawLing
 #1.send request and wait for response - urllib.request
 #2. get the response and fetch the data - bs4(BeautifulSOup4) , LXML - library XML
@@ -9,3 +10,35 @@ data = bs4.BeautifulSoup(response,'lxml')
 div = data.find('div',class_="title_wrapper")
 h1 = div.find('h1')
 print("Movie Name :",h1.text)
+'''
+
+
+#Web CrawLing
+#1.send request and wait for response - urllib.request
+#2. get the response and fetch the data - bs4(BeautifulSOup4) , LXML - library XML
+import urllib.request as url
+import bs4,lxml
+import webbrowser
+movieName = input("Enter Movie Name =>").replace(" ","+")
+path ="https://www.imdb.com/find?q="+movieName
+response = url.urlopen(path)
+data = bs4.BeautifulSoup(response,'lxml')
+result = data.find('td',class_="result_text")
+
+link ='https://www.imdb.com'+result.find('a')['href'] 
+
+response = url.urlopen(link)
+
+data = bs4.BeautifulSoup(response,'lxml')
+div = data.find('div',class_="title_wrapper")
+h1 = div.find('h1')
+print("Movie Name :",h1.text)
+rating = data.find('span',itemprop="ratingValue")
+print("Rating : ",rating.text,'/10')
+a_group = data.find('div',class_='subtext')
+a_list = a_group.findAll('a')
+print("Genre : ",end="")
+for i in a_list[0:len(a_list)-1]:
+    i = i.text
+    print(i,end="  |  ")
+print('\nDate of Release : ',a_list[-1].text)
