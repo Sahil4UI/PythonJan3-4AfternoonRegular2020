4dbe4fbb1f2e73ee3c6c7931b70635d623c318cf
Sahil Kumar <54834362+Sahil4UI@users.noreply.github.com>
2021-02-16 15:24:50 +0530

    Update pygame01.py

--- pygame01.py
+++ pygame01.py
@@ -13,9 +13,11 @@ custom_color = (100,20,40)
 circle_x = 20
 circle_y = 20
 move_x = 1
+move_y = 1
 while True:
     screen.fill(white)
     for event in pygame.event.get():
+        #print(event)
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
@@ -23,4 +25,16 @@ while True:
     #pygame.draw.rect(screen,blue,[W//2-25,H//2-25,50,50])
     pygame.draw.circle(screen,red,[circle_x,circle_y],20)
     circle_x+=move_x
+    circle_y+=move_y
+    
+    if circle_x > W-20:
+        move_x = -1
+    elif circle_x < 20:
+        move_x = 1
+
+    if circle_y > H-20:
+        move_y = -1
+    elif circle_y < 20:
+        move_y = 1
+
     pygame.display.flip()
